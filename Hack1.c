5f54de514d7d984d83cb66e2d298e60f416344ef
Quinn Michael Lennemann <qlennemann@cse.unl.edu>
2018-08-29 22:05:31 -0500

    Quinn's commit

--- Hack1.c
+++ Hack1.c
@@ -1,5 +1,5 @@
 /**
-* Author: Jonathan Anderson
+* Author: Jonathan Anderson, Quinn Lennemann
 * Date: 29 August 2018
 * Assignment: Hack 1.0
 * Goal: Create a message stating me and my partners
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Jonathan Anderson; Music Education\n");
+  printf("Jonathan Anderson; Music Education\nQuinn Lennmann; Computer Science\n");
 
   return 0;
 }
